556265c0b7817939bd7ab4375e13e618b365b0ae
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-04-14 22:47:01 -0400

    made pixel move in diagonal YAYAYYAYAYAY

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Grace
+ * Created on: April 2026
+ * This program moving pixel!!!
 */
 
-basic.showString('Hello, World!')
+//variables 
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    loopCounter = 4
+    sprite = game.createSprite(4, 4)
+
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
